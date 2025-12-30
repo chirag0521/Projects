b7451bf81b863ff57295cd4185427bdf44a45205
chirag0521 <chiragsarda234@gmail.com>
2025-12-30 12:45:01 +0530

    Fully completed

--- DOM-project/script.js
+++ DOM-project/script.js
@@ -137,75 +137,184 @@ function motivationalQuote() {
 }
 motivationalQuote()
 
-function pomodoroTimer(){
+function pomodoroTimer() {
     let timer = document.querySelector('.pomo-timer h1')
-var startBtn = document.querySelector('.pomo-timer .start-timer')
-var pauseBtn = document.querySelector('.pomo-timer .pause-timer')
-var resetBtn = document.querySelector('.pomo-timer .reset-timer')
-var session = document.querySelector('.pomodoro-fullpage .session')
-var isWorkSession = true
-let timerInterval = null
-let totalSeconds = 25 * 60
+    var startBtn = document.querySelector('.pomo-timer .start-timer')
+    var pauseBtn = document.querySelector('.pomo-timer .pause-timer')
+    var resetBtn = document.querySelector('.pomo-timer .reset-timer')
+    var session = document.querySelector('.pomodoro-fullpage .session')
+    var isWorkSession = true
+    let timerInterval = null
+    let totalSeconds = 25 * 60
 
 
-function updateTime() {
-    let minutes = Math.floor(totalSeconds / 60)
-    let seconds = totalSeconds % 60
-    timer.innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
-}
+    function updateTime() {
+        let minutes = Math.floor(totalSeconds / 60)
+        let seconds = totalSeconds % 60
+        timer.innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+    }
 
-function startTimer() {
-    clearInterval(timerInterval)
-    if (isWorkSession) {
-
-
-        timerInterval = setInterval(function () {
-            if (totalSeconds > 0) {
-                totalSeconds--
-                updateTime()
-            } else {
-                isWorkSession = false
-                clearInterval(timerInterval)
-                timer.innerHTML = '05:00'
-                 session.innerHTML = 'Lets take a Break'
-                 session.style.backgroundColor = 'var(--blue)'
-                 totalSeconds = 5 * 60
-            }
-        }, 1000)
-        
-    } else {
+    function startTimer() {
+        clearInterval(timerInterval)
+        if (isWorkSession) {
 
 
-    
-    timerInterval = setInterval(function () {
-        if (totalSeconds > 0) {
-            totalSeconds--
-            updateTime()
+            timerInterval = setInterval(function () {
+                if (totalSeconds > 0) {
+                    totalSeconds--
+                    updateTime()
+                } else {
+                    isWorkSession = false
+                    clearInterval(timerInterval)
+                    timer.innerHTML = '05:00'
+                    session.innerHTML = 'Lets take a Break'
+                    session.style.backgroundColor = 'var(--blue)'
+                    totalSeconds = 5 * 60
+                }
+            }, 1000)
+
         } else {
-            isWorkSession = true
-            clearInterval(timerInterval)
-            timer.innerHTML = '25:00'
-             session.innerHTML = 'Work Session'
-             session.style.backgroundColor = 'var(--green)'
-             totalSeconds = 25 * 60
+
+
+
+            timerInterval = setInterval(function () {
+                if (totalSeconds > 0) {
+                    totalSeconds--
+                    updateTime()
+                } else {
+                    isWorkSession = true
+                    clearInterval(timerInterval)
+                    timer.innerHTML = '25:00'
+                    session.innerHTML = 'Work Session'
+                    session.style.backgroundColor = 'var(--green)'
+                    totalSeconds = 25 * 60
+                }
+
+            }, 1000)
         }
-    
-    }, 1000)
-}
-}
-function pauseTimer() {
-    clearInterval(timerInterval)
-}
-function resetTimer() {
-    totalSeconds = 25 * 60
-    clearInterval(timerInterval)
-    updateTime()
-}
-startBtn.addEventListener('click', startTimer)
-pauseBtn.addEventListener('click', pauseTimer)
-resetBtn.addEventListener('click', resetTimer)
+    }
+    function pauseTimer() {
+        clearInterval(timerInterval)
+    }
+    function resetTimer() {
+        totalSeconds = 25 * 60
+        clearInterval(timerInterval)
+        updateTime()
+    }
+    startBtn.addEventListener('click', startTimer)
+    pauseBtn.addEventListener('click', pauseTimer)
+    resetBtn.addEventListener('click', resetTimer)
 }
 pomodoroTimer()
 
+function weatherReport() {
+    var city = 'Hailakandi'
+    var header1Time = document.querySelector('.header1 h1')
+    var header1Date = document.querySelector('.header1 h2')
+    var header2Temp = document.querySelector('.header2 h2')
+    var header2Condition = document.querySelector('.header2 h4')
+    var feelsLike = document.querySelector('.header2 .feelsLike')
+    var humidity = document.querySelector('.header2 .humidity')
+    var wind = document.querySelector('.header2 .wind')
+    var header = document.querySelector('header')
 
 
+    var data = null
+
+    async function weatherAPIcall() {
+        let apikey = "188159041bdf4e0894804917253012"
+        var response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}`);
+        data = await response.json()
+
+        header2Temp.innerHTML = `${data.current.temp_c}°C`
+        header2Condition.innerHTML = `${data.current.condition.text}`
+        feelsLike.innerHTML = `Feels Like: ${data.current.feelslike_c}°C`
+        humidity.innerHTML = `Humidity: ${data.current.humidity}`
+        wind.innerHTML = `Wind: ${data.current.wind_kph} km/h`
+        console.log(data);
+
+    }
+    weatherAPIcall()
+
+    var date = null
+
+    function timeDate() {
+        var totalDaysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        var totalMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        date = new Date()
+        var dayOfWeek = (totalDaysOfWeek[date.getDay()]);
+        var hours = date.getHours()
+        var minutes = date.getMinutes()
+        var sec = date.getSeconds()
+        var today = date.getDate()
+        var month = date.getMonth()
+        var exactMonth = totalMonths[month]
+        var year = date.getFullYear()
+        header1Date.innerHTML = `${today} ${exactMonth}, ${year}`
+
+        if (hours >= 4 && hours <= 12) {
+            header.style.backgroundImage = `url(./morning.jpg)`
+            header.style.backgroundSize = 'cover';
+            header.style.backgroundPosition = 'center';
+            header.style.backgroundRepeat = 'no-repeat';
+
+        }
+        else if (hours > 12 && hours <= 17) {
+            header.style.backgroundImage = `url(./afternoon.jpg)`
+            header.style.backgroundSize = 'cover';
+            header.style.backgroundPosition = 'center';
+            header.style.backgroundRepeat = 'no-repeat';
+
+        }
+        else if (hours > 17 && hours <= 19) {
+            header.style.backgroundImage = `url(./evening.jpg)`
+            header.style.backgroundSize = 'cover';
+            header.style.backgroundPosition = 'center';
+            header.style.backgroundRepeat = 'no-repeat';
+        }
+        else {
+            header.style.backgroundImage = `url(./night.jpg)`
+            header.style.backgroundSize = 'cover';
+            header.style.backgroundPosition = 'center';
+            header.style.backgroundRepeat = 'no-repeat';
+        }
+        header1Time.innerHTML = `${dayOfWeek}, ${hours}:${String(minutes).padStart(2, "0")}:${String(sec).padStart(2, "0")}`
+
+
+    }
+    setInterval(() => {
+        timeDate()
+    }, 1000)
+}
+weatherReport()
+
+var theme = document.querySelector('.theme')
+
+var flag = 0
+
+var rootElement = document.documentElement
+
+theme.addEventListener('click', function (){
+    if(flag==0){
+    rootElement.style.setProperty('--pri', '#DFD0B8')
+    rootElement.style.setProperty('--sec', '#222831')
+    rootElement.style.setProperty('--tri1', '#948979')
+    rootElement.style.setProperty('--tri2', '#393E46')
+    flag =1
+    }
+    else if(flag==1){
+    rootElement.style.setProperty('--pri', '#F1EFEC')
+    rootElement.style.setProperty('--sec', '#030303')
+    rootElement.style.setProperty('--tri1', '#D4C9BE')
+    rootElement.style.setProperty('--tri2', '#123458')
+    flag = 2
+    }
+    else if(flag==2){
+    rootElement.style.setProperty('--pri', '#F8F4E1')
+    rootElement.style.setProperty('--sec', '#2b1305')
+    rootElement.style.setProperty('--tri1', '#F3BA17')
+    rootElement.style.setProperty('--tri2', '#74512D')
+    flag = 0
+    }
+
+})
