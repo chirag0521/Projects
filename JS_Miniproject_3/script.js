3f9c9a7ee450ebb796d582f267e11a1b50356b96
chirag0521 <chiragsarda234@gmail.com>
2026-01-04 08:16:28 +0530

    completed quiz app

--- JS_Miniproject_3/script.js
+++ JS_Miniproject_3/script.js
@@ -30,7 +30,7 @@ var questions = [
 
 
 var questionElement = document.getElementById("question");
-var answerButton = document.getElementById("answer-buttons");
+var answerButtons = document.getElementById("answer-buttons");
 var nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -44,11 +44,73 @@ function startQuiz(){
 }
 
 function showQuestion(){
+    resetState()
     let currentQuestion= questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". "+ currentQuestion.question;
 
     currentQuestion.answers.forEach(answer=>{
+        var button = document.createElement("button")
+        button.innerHTML = answer.text
+        button.classList.add("btn")
+        answerButtons.appendChild(button)
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
         
     })
 }
+
+function resetState(){
+    nextButton.style.display = "none"
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    var selectedBtn = e.target;
+    var isCorrect = selectedBtn.dataset.correct === "true"
+    if(isCorrect){
+        selectedBtn.classList.add("correct")
+        score++
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerButtons.children).forEach(button=>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true
+    })
+    nextButton.style.display = "block"
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `Your score is ${score} out of ${questions.length}`
+    nextButton.innerHTML = "Play Again!"
+    nextButton.style.display = "block"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion()
+    }
+    else{
+        showScore()
+    }
+}
+
+nextButton.addEventListener('click', ()=>{
+    if(currentQuestionIndex<questions.length){
+        handleNextButton()
+    }
+    else{
+        startQuiz()
+    }
+})
+
+startQuiz()
